d6bc474746367a9d2e96b0c63dbd8f70146a4115
Cieszkowski Kamil <kamil.cieszkowski@streamsoft.pl>
2023-07-31 16:35:40 +0200

    Project init

---
+++ app/src/main/java/pl/team/finap/MainActivity.kt
@@ -0,0 +1,21 @@
+package pl.team.finap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pl.team.finap.ui.theme.FinAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
