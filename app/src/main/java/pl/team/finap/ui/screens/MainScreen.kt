74cf33f66a097dcb98fc7076dbb80610d680d89e
kamil.cieszkowski <cieszkens@gmail.com>
2023-08-29 19:08:50 +0200

    Create final look of the MainScreen

--- app/src/main/java/pl/team/finap/ui/screens/MainScreen.kt
+++ app/src/main/java/pl/team/finap/ui/screens/MainScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -25,12 +24,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
-import com.himanshoe.charty.bar.model.BarData
-import com.himanshoe.charty.common.ChartDataCollection
 import pl.team.finap.R
 
 @Composable
@@ -38,21 +39,27 @@ fun MainScreen() {
     val navController = rememberNavController()
     val purpleToWhiteGradient = Brush.linearGradient(
         colors = listOf(
-            Color(R.color.weirdGray),
-            Color.Black
+            Color(R.color.weirdGray), Color(R.color.gradient_end_black)
         )
     )
-    // Replace this with your actual data
+    val data = listOf(10f, 50f, 80f, 40f, 100f)
+    val gradientColors = listOf(
+        listOf(
+            Color(R.color.gradient_start_red), Color(R.color.gradient_end_pink)
+        ), listOf(
+            Color(R.color.gradient_start_green), Color(R.color.gradient_end_lime)
+        ), listOf(
+            Color(R.color.gradient_start_blue), Color(R.color.gradient_end_light_blue)
+        ), listOf(
+            Color(R.color.gradient_start_yellow), Color(R.color.gradient_end_orange)
+        ), listOf(
+            Color(R.color.gradient_start_purple), Color(R.color.gradient_end_magenta)
+        ), listOf(
+            Color(R.color.gradient_start_cyan), Color(R.color.gradient_end_light_cyan)
+        )
+    )
     val walletBalance = "100.00"
 
-    val chartDataCollection: ChartDataCollection =
-        ChartDataCollection(
-            listOf(
-                BarData(25f, 0f, Color.Gray),
-                BarData(30f, 0f, Color.Green),
-                BarData(10f, 0f, Color.Blue)
-            )
-        )
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -60,22 +67,40 @@ fun MainScreen() {
             .padding(8.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
+            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
             Row(
-                modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = "Wallet Icon",
-                    tint = Color.Black
-                )
-                Text(
-                    text = walletBalance,
-                    style = MaterialTheme.typography.titleLarge
-                )
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Balance", style = TextStyle(
+                                fontWeight = FontWeight.Bold, fontSize = 36.sp
+                            ), modifier = Modifier.padding(2.dp, 4.dp)
+                        )
+                    }
+                    Row() {
+                        Text(
+                            text = walletBalance, style = TextStyle(
+                                fontStyle = FontStyle.Italic, fontSize = 24.sp
+                            ), modifier = Modifier.padding(2.dp, 4.dp)
+                        )
+                        Text(
+                            text = "dollars", style = TextStyle(
+                                fontStyle = FontStyle.Italic, fontSize = 24.sp
+                            ), modifier = Modifier.padding(2.dp, 4.dp)
+                        )
+                    }
+
+                }
+
             }
 
             Card(
@@ -84,46 +109,84 @@ fun MainScreen() {
                     .height(250.dp)
                     .background(Color.Gray.copy(alpha = 0.2f))
             ) {
-                val data = listOf(10f, 50f, 80f, 40f, 100f, 60f)
-                BarChart(data = data, maxHeight = 200.dp)
+                BarChart(data = data, gradientColors = gradientColors, maxHeight = 200.dp)
             }
-            Row() {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(100.dp)
-                        .padding(5.dp)
-                        .weight(1f)
-                ) {
-                    Text(text = "Wydatki")
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(100.dp)
-                        .padding(5.dp)
-                        .weight(1f)
-                ) {
-                    Text(text = "Przychody")
-                }
-            }
-            FloatingActionButton(
-                onClick = {
-                    navController.navigate("Destination")
-                },
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.End)
-            ) {
-                Icon(Icons.Default.Add, contentDescription = "Add New Transaction")
-            }
+            TransactionRow()
+
+        }
+        FloatingActionButton(
+            onClick = {
+                navController.navigate("Destination")
+            }, modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Add New Transaction")
+        }
+    }
+}
+
+@Composable
+fun TransactionRow() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TransactionCard(
+            label = "Expenses",
+            amount = String.format("-%s", "12091"),
+            color = Color(R.color.gradient_start_red),
+            modifier = Modifier
+                .weight(1f)
+                .padding(5.dp)
+        )
+        TransactionCard(
+            label = "Income",
+            amount = String.format("+%s", "12091"),
+            color = Color(R.color.gradient_start_green),
+            modifier = Modifier
+                .weight(1f)
+                .padding(5.dp)
+        )
+    }
+}
+
+@Composable
+fun TransactionCard(
+    label: String,
+    amount: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp)
+    ) {
+        Column() {
+            Text(
+                text = label,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+                modifier = Modifier.padding(2.dp, 4.dp)
+            )
+            Text(
+                text = amount,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    color = color
+                ),
+                modifier = Modifier.padding(10.dp, 4.dp)
+            )
         }
     }
 }
 
 
 @Composable
-fun BarChart(data: List<Float>, maxHeight: Dp) {
+fun BarChart(data: List<Float>, gradientColors: List<List<Color>>, maxHeight: Dp) {
     Box(modifier = Modifier.fillMaxWidth()) {
         val maxDataValue = data.maxOrNull() ?: 1f
 
@@ -133,8 +196,11 @@ fun BarChart(data: List<Float>, maxHeight: Dp) {
                 .background(color = MaterialTheme.colorScheme.surface),  // Set the color to match Card
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            data.forEach { value ->
+            data.forEachIndexed { index, value ->
                 val heightPercentage = value / maxDataValue
+                val barGradient =
+                    Brush.verticalGradient(colors = gradientColors[index % gradientColors.size])
+
                 Box(
                     modifier = Modifier
                         .weight(1f)
@@ -146,7 +212,7 @@ fun BarChart(data: List<Float>, maxHeight: Dp) {
                             .align(Alignment.BottomStart)
                             .fillMaxWidth()
                             .height((maxHeight * heightPercentage))
-                            .background(Color.Blue)
+                            .background(brush = barGradient)
                     )
                 }
                 Spacer(
